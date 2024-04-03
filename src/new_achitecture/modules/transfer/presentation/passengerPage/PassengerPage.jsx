ec7114581f23a6cd302e1d8a022d39c74f0c424c
arkadii <arkadii.malygin@gmail.com>
2024-04-03 17:42:43 +0300

    disable currently opened links (transferBuses)

--- src/new_achitecture/modules/transfer/presentation/passengerPage/PassengerPage.jsx
+++ src/new_achitecture/modules/transfer/presentation/passengerPage/PassengerPage.jsx
@@ -1,80 +1,81 @@
-import { Box, Container, LinearProgress, Typography } from '@material-ui/core';
+import {Box, Container, LinearProgress, Typography} from '@material-ui/core';
 import i18next from 'i18next';
 import Transfer from '../transferPage/Transfer';
 import filtersClasses from '../../../../modules/trip_search/lib/filterSearch/Filter/FilterComponent.module.css';
 import classes from './PassengerPage.module.css';
 import TransferCardComponent from '../transferPage/transferCard/TransferCardComponent';
-import { LoadingButton } from '@mui/lab';
-import { Alert } from '@mui/material';
+import {LoadingButton} from '@mui/lab';
+import {Alert} from '@mui/material';
 import i18n from '../../../../../i18n';
-import { PAGE_SIZE } from '../../../trip_search/data/api/data-service';
+import {PAGE_SIZE} from '../../../trip_search/data/api/data-service';
 import FiltersCitiesFrom from '../../../trip_search/lib/filterSearch/Filter/FiltersCitiesFrom';
 import usePassenger from '../hooks/usePassenger';
 import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
 
 function isNewDesign() {
-  return (
-    process.env.REACT_APP_NEW_DESIGN &&
-    process.env.REACT_APP_NEW_DESIGN === 'true'
-  );
+    return (
+        process.env.REACT_APP_NEW_DESIGN &&
+        process.env.REACT_APP_NEW_DESIGN === 'true'
+    );
 }
 
 export default function PassengerPage() {
-  const { data, addNextHandler, loading } = usePassenger();
+    const {data, addNextHandler, loading} = usePassenger();
 
-  const handleClick = () => {
-    addNextHandler();
-  };
+    const handleClick = () => {
+        addNextHandler();
+    };
 
-  return (
-    <Container maxWidth='xl' className={classes.tb_padding}>
+    return (
+        <Container maxWidth='xl' className={classes.tb_padding}>
 
-      <div className={filtersClasses.filters_sector}>
-          <div style={{marginBottom: '15px'}}>
-              <Link to={'/viewRoutes'} className={classes.btn}>I'm a passenger</Link>
-              <Link to={'/addTransfer'} className={classes.btn}>I'm a carrier</Link>
-          </div>
+            <div style={{marginBottom: '15px'}}>
+                <Link to={'#'} className={classes.disabled}>I'm a passenger</Link>
+                <Link to={'/addTransfer'} className={classes.btn}>I'm a carrier</Link>
+            </div>
 
-        <Typography variant='button' display='block' gutterBottom>
-          {i18next.t('Filter')}
-        </Typography>
-        <FiltersCitiesFrom />
-      </div>
-      {/* <Divider style={{margin: "10px"}}/> */}
-      {/* {loading && <h2>Loading...</h2>} */}
-      {/* {!loading && <transfersList transfers={transfers} />} */}
-      <div className={classes.transfers}>
-        {loading.isLoadingTransfers ? (
-          <Box sx={{ width: '100%' }}>
-            <LinearProgress />
-          </Box>
-        ) : // <transfersList transfers={data.transfers} />
-        isNewDesign() ? (
-          data.transfers.map((transfer, i) => (
-            <TransferCardComponent key={i} transfer={transfer} />
-          ))
-        ) : (
-          data.transfers.map((transfer, i) => (
-            <Transfer key={i} transfer={transfer} />
-          ))
-        )}
-        {data.transfers.length === 0 && data.isFilterApply && (
-          <Alert severity='warning'>{i18n.t('NothingFound')}</Alert>
-        )}
-      </div>
-      <div style={{ width: '200px', margin: '10px auto', textAlign: 'center' }}>
-        {data.nextTransfers.length === PAGE_SIZE &&
-          data.transfers.length !== 0 &&
-          !data.isFilterApply && (
-            <LoadingButton
-              variant='contained'
-              loading={loading.isLoadingNextTransfers}
-              onClick={handleClick}
-            >
-              {i18n.t('LoadMore')}
-            </LoadingButton>
-          )}
-      </div>
-    </Container>
-  );
+            <div className={filtersClasses.filters_sector}>
+
+
+                <Typography variant='button' display='block' gutterBottom>
+                    {i18next.t('Filter')}
+                </Typography>
+                <FiltersCitiesFrom/>
+            </div>
+
+            <div className={classes.transfers}>
+                {loading.isLoadingTransfers ? (
+                        <Box sx={{width: '100%'}}>
+                            <LinearProgress/>
+                        </Box>
+                    ) :
+                    isNewDesign() ? (
+                        data.transfers.map((transfer, i) => (
+                            <TransferCardComponent key={i} transfer={transfer}/>
+                        ))
+                    ) : (
+                        data.transfers.map((transfer, i) => (
+                            <Transfer key={i} transfer={transfer}/>
+                        ))
+                    )}
+                {data.transfers.length === 0 && data.isFilterApply && (
+                    <Alert severity='warning'>{i18n.t('NothingFound')}</Alert>
+                )}
+            </div>
+            <div style={{width: '200px', margin: '10px auto', textAlign: 'center'}}>
+                {data.nextTransfers.length === PAGE_SIZE &&
+                    data.transfers.length !== 0 &&
+                    !data.isFilterApply && (
+                        <LoadingButton
+                            variant='contained'
+                            loading={loading.isLoadingNextTransfers}
+                            onClick={handleClick}
+                        >
+                            {i18n.t('LoadMore')}
+                        </LoadingButton>
+                    )}
+            </div>
+        </Container>
+    );
 }
