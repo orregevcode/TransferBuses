2d8d000c8931ac3ffccf61012d9569c8b300762c
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2024-04-13 05:52:41 +0300

    Add dynamic gallery to the "Travel Tips" section and the "Contacts" section

--- src/new_achitecture/modules/budgetTravelTips/pages/BudgetTravelTipsStartPage.js
+++ src/new_achitecture/modules/budgetTravelTips/pages/BudgetTravelTipsStartPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FindBudget from "../presentation/ButtonsComponent/Find&Budget";
 import Cities from "../presentation/BTTStartComponent/CitiesComponent";
-import GalleryDynamicComponent from "../presentation/BTTStartComponent/GalleryDynamicComp/GalleryDynamicComponent";
 import GalleryComponent from "../presentation/BTTStartComponent/GalleryComponent";
 
 const BudgetTravelTipsStartPage = () => {
